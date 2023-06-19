d41cd10cabdde65a0f2017015b7d21e3f00d5ee1
aissam0011 <aissam.alaarabiou@gmail.com>
2023-06-19 05:07:18 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Description: 'prints the line with puts function'
+ * return: Always 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle")
+return (0);
+}
