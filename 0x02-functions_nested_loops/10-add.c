87363c41e925f3a0152a1a093afb5436f71ac465
aissam0011 <aissam.alaarabiou@gmail.com>
2023-06-20 11:16:38 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*add - adds two integers and returns the result
+*@n : the first number to be computed
+*@m : the first number to be computed
+*Return: integer value
+*/
+int add(int n, int m)
+{
+return (n + m);
+}
