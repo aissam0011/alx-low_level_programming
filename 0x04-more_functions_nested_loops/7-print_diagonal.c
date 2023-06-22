df11661daa3b1ce2b5069d37ddc04d93a4fa5e53
aissam0011 <aissam.alaarabiou@gmail.com>
2023-06-22 13:04:08 -0700

    git commit -m

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,12 +4,14 @@
 *@n: input variable
 *Return: void
 */
-void print_diagonal(int n);
+void print_diagonal(int n)
 {
 int i, j;
 if (n <= 0)
 _putchar('\n');
 for (i = 0; j < i; j++)
+{
+for (j = 0; j < i; j++)
 {
 _putchar(' ');
 }
