57595c410c54336cf0c9a20244ccb7da88ba5f27
aissam0011 <aissam.alaarabiou@gmail.com>
2023-07-13 10:50:39 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,6 @@
-#ifndef MAIN_H		
-#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 char *create_array(unsigned int size, char c);		
 int _putchar(char c);		
 char *_strdup(char *str);		
